9ee5623033ee3e9dd9ba1a1186c81836353b0a51
unknown <haoguan200@gmail.com>
2020-10-16 20:39:26 -0700

    orm.js added

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-### Schema
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE burgers_db;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT auto_increment PRIMARY KEY,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN default false
 );
