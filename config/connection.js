7ceda415e775146d6fe8322aa93de1c29a4b8233
unknown <haoguan200@gmail.com>
2020-10-16 21:31:04 -0700

    handle bars modified

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,14 @@ var connection = mysql.createConnection(process.env.JAWS_DB || {
   database: "cat_db"
 });
 
+/* var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+}); */
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
